83b73007bc014bf1e6f94df5590b5409c97552b8
Alejandro Morales <almoral@Alejandro-Moraless-iMac-2.local>
2011-03-24 13:22:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentPics::Application.config.secret_token = '114dcc99a2f8a5627294513f67b7fac9b08b1481a7f6ea297e7acc1a6dae236434741daf40fc3a36a9a6ad4fddac45ee4412c9be1b442a5ad8e0816d94be9da3'
